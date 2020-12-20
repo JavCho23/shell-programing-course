df02021540bd2de7e7ca75bb74c597653ac5554f
Javier Chávez <javier120699lili@gmail.com>
2020-12-20 16:45:17 -0500

    Using while

--- options.sh
+++ options.sh
@@ -1,2 +1,3 @@
+# !/bin/bash
 # Para las opciones y variables un buen recurso es el siguiente enlace
-# https://likegeeks.com/es/scripting-de-bash-parametros-y-opciones/
+echo https://likegeeks.com/es/scripting-de-bash-parametros-y-opciones/
---
+++ while.sh
@@ -0,0 +1,10 @@
+# Porgrama para practicar la sentencia while
+
+number=0
+
+while [ $number -le 100 ]
+do 
+        echo "Hola por vez número $number"
+        number=$((number + 1))
+done
+
