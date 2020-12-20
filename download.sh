ccdb2b6eb831c7aa20a23185e7b832d35bfd530f
Javier Chávez <javier120699lili@gmail.com>
2020-12-19 22:59:48 -0500

    Download files

---
+++ download.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de informacion
+
+echo "Descargar información de internet"
+
+wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.61/bin/apache-tomcat-8.5.61.zip
